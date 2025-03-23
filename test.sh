5bf6baf6108925ae4ec6f6e8548f11e3ecb4928a
github-newstar <rss467301127@gmail.com>
2025-03-23 11:21:35 +0000

    update

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 # 运行测试并将输出同时写入标准输出和日志文件
 echo "Starting tests at $(date)"
 cd /test
+touch /logs/py-test-logs.log /logs/wrk-test-logs.log
 # curl -sSf https://astral.sh/uv/install.sh | sh 
 /root/.local/bin/uv run test_redis.py 2>&1 | tee -a /logs/py-test-logs.log
 tail -n 15 /tmp/full_output.log > /logs/py-test-logs.lo
